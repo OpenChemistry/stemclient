169b4864b1e5b0362f8a82c1e6f71f1b0c4e2eef
Chris Harris <chris.harris@kitware.com>
2018-12-14 16:33:49 -0500

    Aggregate partial images in client

--- src/App.js
+++ src/App.js
@@ -6,26 +6,43 @@ import './App.css';
 import STEMImage from './STEMImage.js'
 import openSocket from 'socket.io-client';
 
+const NUMBER_OF_PIXELS = 160*160;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.stemData = new Float64Array(NUMBER_OF_PIXELS);
+    this.images = 1;
   }
 
   componentDidMount() {
     this.socket = openSocket('http://localhost:5000/stem');
     this.socket.on('connect', () => {
-      console.log('connected');
+      console.log('Connected');
     });
 
-    this.socket.on('stem', (data) => {
-      this.setState(data);
+    this.socket.on('stem', (msg) => {
+      const stemData = new Float64Array(msg.data);
+
+      // Aggregate the values
+      for(let i=0; i<NUMBER_OF_PIXELS; i++) {
+        this.stemData[i] += stemData[i];
+      }
+
+      // Once we have aggregated all the values set the state so the STEMImage
+      // gets generated.
+      if (this.images == 32) {
+        this.setState({
+          data: this.stemData
+        });
+      }
+
+      this.images +=1;
     })
   }
 
   render() {
-    console.log(this.state.data);
     return (
       <div className="App">
         <STEMImage data={this.state.data} />
