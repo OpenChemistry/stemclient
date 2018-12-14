066cc1e54d63fb8d053b3970444bf98f23ed344e
Chris Harris <chris.harris@kitware.com>
2018-12-14 14:55:08 -0500

    Receive data version SocketIO

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {load} from './load.js';
 
 import STEMImage from './STEMImage.js'
+import openSocket from 'socket.io-client';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
- 
-  
+
   componentDidMount() {
-    load(arrayBuffer =>
-      this.setState({ data: arrayBuffer }));
-  }	
+    this.socket = openSocket('http://localhost:5000/stem');
+    this.socket.on('connect', () => {
+      console.log('connected');
+    });
+
+    this.socket.on('stem', (data) => {
+      this.setState(data);
+    })
+  }
 
   render() {
     console.log(this.state.data);
