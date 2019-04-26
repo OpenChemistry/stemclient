e07722153e4c25b381fd242c56e3623cedc79a32
Alessandro Genova <alessandro.genova@kitware.com>
2019-04-26 09:43:05 -0400

    Add basic colormap

--- src/components/stem-image/index.tsx
+++ src/components/stem-image/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { ImageDataSource } from '../../stem-image/data';
 import { ImageView } from '../../stem-image/view';
+import { Vec3 } from '../../stem-image/types';
 
 interface Props {
   source: ImageDataSource;
+  colorMap?: Vec3[];
 }
 
 export default class STEMImage extends React.Component<Props> {
@@ -13,11 +15,26 @@ export default class STEMImage extends React.Component<Props> {
   componentDidMount() {
     const { source } = this.props;
     this.imageView = new ImageView(this.containerRef.current!, source);
+    this.updateColorMap();
+  }
+
+  componentWillUpdate(prevProps: Props) {
+    const { colorMap } = this.props;
+    if (colorMap !== prevProps.colorMap) {
+      this.updateColorMap();
+    }
   }
 
   componentWillUnmount() {
     if (this.imageView) {
       this.imageView.unsubscribe();
+    }
+  }
+
+  updateColorMap() {
+    const { colorMap } = this.props;
+    if (this.imageView && colorMap) {
+      this.imageView.setColorMap(colorMap);
     }
   }
 
